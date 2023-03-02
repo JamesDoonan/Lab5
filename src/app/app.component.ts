893e6b12288c6dee7b91b3c4abc76a74e07db199
unknown <james57doonan@gmail.com>
2023-03-02 15:37:29 +0000

    Lab5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-lab';
+  myVar: boolean = true;
+  count:number = 0;
+
+  presidents: string[]= ["Higgins","McAleese","Robinson","Hillery"];
+  president: any[] =
+  [{name: "Higgins", term:"2011-"},
+  {name: "McAleese", term:"1997-2011"},
+  {name: "Robinson", term:"1990-1997"},
+  {name: "Hillery", term:"1976-1990"},
+  {name: "O Dalaigh", term:"1974-1976"}];
+
+  onClick(){
+    this.count++;
+  }
+
+  onDoubleClick() {
+    if(this.myVar === false) {
+      this.myVar = true;
+    }
+    else {
+      this.myVar = false;
+    }
+
+  }
 }
